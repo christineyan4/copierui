2b593a5f42908df2bf63ec9ddafabd31c7cdd3a2
U-LAPTOP-AKPC20\AK <AK Alilonu>
2019-11-14 00:51:08 -0600

    added highlighting

--- copy.js
+++ copy.js
@@ -1,12 +1,32 @@
 LEFTMOST = 30
 BELOW_BUTTON = 40
 
+fillColor = 255;
+scanSingleColor = 100;
+scanDoubleColor = fillColor;
+printSingleColor = 100;
+printDoubleColor = fillColor;
+
 function setup() {
   createCanvas(1600, 1600);
   background(200);
+  
   numCopiesInput = createInput('1');
   numCopiesInput.position(420, BELOW_BUTTON + 50);
+  
 }
+
+function scanToggle() {
+    scanSingleColorCopy = scanSingleColor;
+    scanSingleColor = scanDoubleColor;
+    scanDoubleColor = scanSingleColorCopy;
+    }
+
+function printToggle() {
+    printSingleColorCopy = printSingleColor;
+    printSingleColor = printDoubleColor;
+    printDoubleColor = printSingleColorCopy;
+    }
 
 function minus() {
     v = numCopiesInput.value();
@@ -63,23 +83,39 @@ function copy1() {
     }
 
 function draw() {
-    fill(255);
+    fill(fillColor);
     textSize(24);
     textAlign(CENTER, CENTER);
-    
+
     // SCAN TYPE
     text("SCAN TYPE", LEFTMOST + 100, 50);
     scanSingleButton = createButton('Single-sided');
     scanSingleButton.position(LEFTMOST, BELOW_BUTTON + 50);
+    fill(scanSingleColor);
+    rect(scanSingleButton.x - 1, scanSingleButton.y - 1, scanSingleButton.width + 2, scanSingleButton.height + 2);
+    scanSingleButton.mousePressed(scanToggle);
     scanDoubleButton = createButton('Double-sided');
     scanDoubleButton.position(LEFTMOST + 100, BELOW_BUTTON + 50);
-    
+    fill(scanDoubleColor);
+    rect(scanDoubleButton.x - 1, scanDoubleButton.y - 1, scanDoubleButton.width + 2, scanDoubleButton.height + 2)
+    scanDoubleButton.mousePressed(scanToggle);
+
+    fill(fillColor);
+
     // PRINT TYPE
     text("PRINT TYPE", LEFTMOST + 100, 150);
     printSingleButton = createButton('Single-sided');
     printSingleButton.position(LEFTMOST, BELOW_BUTTON + 150);
+    fill(printSingleColor);
+    rect(printSingleButton.x - 1, printSingleButton.y - 1, printSingleButton.width + 2, printSingleButton.height + 2);
+    printSingleButton.mousePressed(printToggle);
     printDoubleButton = createButton('Double-sided');
     printDoubleButton.position(LEFTMOST + 100, BELOW_BUTTON + 150);
+    fill(printDoubleColor);
+    rect(printDoubleButton.x - 1, printDoubleButton.y - 1, printDoubleButton.width + 2, printDoubleButton.height + 2)
+    printDoubleButton.mousePressed(printToggle);
+
+    fill(fillColor);
 
     // MORE OPTIONS
     moreOptionsButton = createButton('MORE OPTIONS');
@@ -125,6 +161,6 @@ function draw() {
 
     // COPY
     copyButton = createButton('COPY');
-    copyButton.position(400, 300);
+    copyButton.position(numCopiesInput.x + numCopiesInput.width/2 - copyButton.width/2, moreOptionsButton.y);
     copyButton.mousePressed(copy1);
 }
--- more_options.js
+++ more_options.js
@@ -1,6 +1,18 @@
 LEFTMOST = 30
 BELOW_BUTTON = 30
 
+fillColor = 255;
+brightnessColor = 0;
+paperSourceAColor = 100;
+paperSourceBColor = fillColor;
+paperSourceCColor = fillColor;
+separatorPageOffColor = 100;
+separatorPageOnColor = fillColor;
+separatorPageOn = false;
+separatorPageSourceAColor = fillColor;
+separatorPageSourceBColor = fillColor;
+separatorPageSourceCColor = fillColor;
+
 function setup() {
   createCanvas(1600, 1600);
   background(200);
@@ -8,6 +20,58 @@ function setup() {
   slider.position(LEFTMOST + 300, 50);
   slider.style('width', '80px');
 }
+
+function paperSourceToggle() {
+    paperSourceAColor = fillColor;
+    paperSourceBColor = fillColor;
+    paperSourceCColor = fillColor;
+    paperSource = this.name;
+    switch (this.name) {
+        case 'A':
+            paperSourceAColor = 100;
+            break;
+        case 'B':
+            paperSourceBColor = 100;
+            break;
+        case 'C':
+            paperSourceCColor = 100;
+            break;
+        }
+    }
+
+function separatorPageToggle() {
+    separatorPageOffColorCopy = separatorPageOffColor;
+    separatorPageOffColor = separatorPageOnColor;
+    separatorPageOnColor = separatorPageOffColorCopy;
+    separatorPageOn = !separatorPageOn;
+
+    if (separatorPageOn) {
+        separatorPageSourceAColor = 100;
+        }
+    else { 
+        separatorPageSourceAColor = fillColor;
+        separatorPageSourceBColor = fillColor;
+        separatorPageSourceCColor = fillColor;
+        }
+    }
+
+function separatorPageSourceToggle() {
+    separatorPageSourceAColor = fillColor;
+    separatorPageSourceBColor = fillColor;
+    separatorPageSourceCColor = fillColor;
+    separatorPageSource = this.name;
+    switch (this.name) {
+        case 'A':
+            separatorPageSourceAColor = 100;
+            break;
+        case 'B':
+            separatorPageSourceBColor = 100;
+            break;
+        case 'C':
+            separatorPageSourceCColor = 100;
+            break;
+        }
+    }
 
 function cancel() {
     alert("Cancelling ...");
@@ -20,41 +84,81 @@ function save1() {
     }
 
 function draw() {
-    fill(255);
+    fill(fillColor);
     textSize(24);
     textAlign(LEFT, TOP);
 
-    let val = slider.value();
-    background(val);
+    brightnessColor = slider.value();
+    //background(fillColor);
 
     
     // BRIGHTNESS
     text("BRIGHTNESS", LEFTMOST, 50);
-    
+    fill(brightnessColor);
+    rect(LEFTMOST + 200, 53, 80, 15);
+
+    fill(fillColor);
+
     // PAPER SOURCE
     text("PAPER SOURCE", LEFTMOST, 150);
     paperSourceAButton = createButton('A');
     paperSourceAButton.position(LEFTMOST, BELOW_BUTTON + 150);
+    fill(paperSourceAColor);
+    rect(paperSourceAButton.x - 1, paperSourceAButton.y - 1, paperSourceAButton.width + 2, paperSourceAButton.height + 2);
+    paperSourceAButton.name = 'A';
+    paperSourceAButton.mousePressed(paperSourceToggle);
     paperSourceBButton = createButton('B');
     paperSourceBButton.position(LEFTMOST + 100, BELOW_BUTTON + 150);
+    fill(paperSourceBColor);
+    rect(paperSourceBButton.x - 1, paperSourceBButton.y - 1, paperSourceBButton.width + 2, paperSourceBButton.height + 2);
+    paperSourceBButton.name = 'B';
+    paperSourceBButton.mousePressed(paperSourceToggle);
     paperSourceCButton = createButton('C');
     paperSourceCButton.position(LEFTMOST + 200, BELOW_BUTTON + 150);
+    fill(paperSourceCColor);
+    rect(paperSourceCButton.x - 1, paperSourceCButton.y - 1, paperSourceCButton.width + 2, paperSourceCButton.height + 2);
+    paperSourceCButton.name = 'C';
+    paperSourceCButton.mousePressed(paperSourceToggle);
+
+    fill(fillColor);
 
     // SEPARATOR PAGE
     text("SEPARATOR PAGE", LEFTMOST, 250);
     separatorPageOffButton = createButton('OFF');
     separatorPageOffButton.position(LEFTMOST, BELOW_BUTTON + 250);
+    fill(separatorPageOffColor);
+    rect(separatorPageOffButton.x - 1, separatorPageOffButton.y - 1, separatorPageOffButton.width + 2, separatorPageOffButton.height + 2);
+    separatorPageOffButton.mousePressed(separatorPageToggle);
     separatorPageOnButton = createButton('ON');
     separatorPageOnButton.position(LEFTMOST + 100, BELOW_BUTTON + 250);
+    fill(separatorPageOnColor);
+    rect(separatorPageOnButton.x - 1, separatorPageOnButton.y - 1, separatorPageOnButton.width + 2, separatorPageOnButton.height + 2);
+    separatorPageOnButton.mousePressed(separatorPageToggle);
+
+    fill(fillColor);
 
     // SEPARATOR PAGE SOURCE
     text("SEPARATOR PAGE SOURCE", LEFTMOST, 350);
     separatorPageSourceAButton = createButton('A');
     separatorPageSourceAButton.position(LEFTMOST, BELOW_BUTTON + 350);
+    fill(separatorPageSourceAColor);
+    rect(separatorPageSourceAButton.x - 1, separatorPageSourceAButton.y - 1, separatorPageSourceAButton.width + 2, separatorPageSourceAButton.height + 2);
+    separatorPageSourceAButton.name = 'A';
+    separatorPageSourceAButton.mousePressed(separatorPageSourceToggle);
     separatorPageSourceBButton = createButton('B');
     separatorPageSourceBButton.position(LEFTMOST + 100, BELOW_BUTTON + 350);
+    fill(separatorPageSourceBColor);
+    rect(separatorPageSourceBButton.x - 1, separatorPageSourceBButton.y - 1, separatorPageSourceBButton.width + 2, separatorPageSourceBButton.height + 2);
+    separatorPageSourceBButton.name = 'B';
+    separatorPageSourceBButton.mousePressed(separatorPageSourceToggle);
     separatorPageSourceCButton = createButton('C');
     separatorPageSourceCButton.position(LEFTMOST + 200, BELOW_BUTTON + 350);
+    fill(separatorPageSourceCColor);
+    rect(separatorPageSourceCButton.x - 1, separatorPageSourceCButton.y - 1, separatorPageSourceCButton.width + 2, separatorPageSourceCButton.height + 2);
+    separatorPageSourceCButton.name = 'C';
+    separatorPageSourceCButton.mousePressed(separatorPageSourceToggle);
+
+    fill(fillColor);
 
     // CANCEL
     cancelButton = createButton('CANCEL');
@@ -63,6 +167,6 @@ function draw() {
 
     // SAVE
     saveButton = createButton('SAVE');
-    saveButton.position(LEFTMOST + 370, 450);
+    saveButton.position(cancelButton.x + 300, 450);
     saveButton.mousePressed(save1);
 }
